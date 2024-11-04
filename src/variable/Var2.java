c824cdc7c6bf9cfc21ffbf52e955559335849248
HaramSs <haram8274@gmail.com>
2024-11-04 20:53:06 +0900

    section3-3

---
+++ src/variable/Var2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a; // 변수 담는 그릇 = 변수 선언
+        a = 10; // 변수 초기화 (10 -> 20)
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
